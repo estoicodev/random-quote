75f84a60a67826ef5f90a51da77d7b520062713f
Mauricio2802 <vmcarrasco2810@gmail.com>
2021-12-22 15:04:22 -0500

    Change keypress to new random quote to S key, and add a bg to body

--- scripts/randomQuote.js
+++ scripts/randomQuote.js
@@ -10,6 +10,11 @@ const quotesArray = [
   "Sé tan bueno que no puedan ignorarte",
   "El que crea y domina su propio proceso de aprendizaje se vuelve un genio a su manera",
   "La mayoría de la gente no empieza. La mayoría de las personas que comienzan no continúan. La mayoría de las personas que continúan se rinden. Muchos ganadores son los últimos en pie",
+  "Una visión sin una tarea es sólo un sueño. Una tarea sin una visión es un trabajo fatigoso. Pero cuando la visión y la tarea van de la mano son la esperanza del mundo",
+  "El progreso humano se debe a las personas que lograron un avance personal con relación al arte, la ciencia o la tecnología, y que fueron capaces de compartir su propia transformación con los demás humanos",
+  "Pensar es la esencia del trabajo y de la vida, y también una de las cosas más difíciles de hacer. Las personas que construyen imperios dedican horas y horas al trabajo mental… mientras los demás se divierten",
+  "Cuando tienes miedo no puedes pensar con claridad; por el contrario, la confianza en tus cualidades te hace mucho más persistente",
+  "La mejor forma de evitar la etapa autónoma y estancamiento satisfactorio es practicando los fallos",
 ];
 const quotesLength = quotesArray.length;
 console.log(`Cantidad de Frases: ${quotesLength}`);
@@ -26,7 +31,7 @@ function newRandomQuote() {
 
 buttonRandomQuote.addEventListener("click", newRandomQuote);
 document.addEventListener("keydown", function (e) {
-  if (e.ctrlKey && e.key === "Enter") {
+  if (e.key === "s") {
     newRandomQuote();
   }
 });
